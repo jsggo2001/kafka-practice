1d9f41ba68fbfbf5e31af151670efcf706d4fcfe
jsggo2001 <jsggo2001@naver.com>
2024-09-02 15:24:46 +0900

    기존 jpa기반 join로직 kafka 방식으로 변환 을 위한 틀 작성

---
+++ src/main/java/com/example/kakfaTest/member/infrastructure/persistence/KafkaMemberProducerAdapter.java
@@ -0,0 +1,20 @@
+package com.example.kakfaTest.member.infrastructure.persistence;
+
+import com.example.kakfaTest.member.domain.Member;
+import com.example.kakfaTest.member.domain.repository.MemberRepository;
+import com.example.kakfaTest.member.infrastructure.persistence.kafka.MemberProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class KafkaMemberProducerAdapter implements MemberRepository {
+
+    private final MemberProducer memberProducer;
+
+    @Override
+    public Member save(Member member) {
+        return memberProducer.saveProduce(member);
+    }
+
+}
---
+++ src/main/java/com/example/kakfaTest/member/infrastructure/persistence/kafka/MemberProducer.java
@@ -0,0 +1,19 @@
+package com.example.kakfaTest.member.infrastructure.persistence.kafka;
+
+import com.example.kakfaTest.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class MemberProducer {
+
+
+
+    public Member saveProduce(Member member) {
+        // 프로듀싱 로직 추가
+
+        return null;
+    }
+}
